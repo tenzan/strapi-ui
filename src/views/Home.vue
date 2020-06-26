466f8ed6d9945fc2cc50b05f5eb41bdb22e4a66c
Askar Karasaev <askar.karasaev@gmail.com>
2020-06-26 15:06:44 +0900

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
+    <ul>
+      <li v-for="location in locations" :key="location.code">
+        <p>{{location.code}}  {{location.name_jp}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      locations: {
+        code: '',
+        name_jp: ''
+      }
+    }
+  },
+  created() {
+    axios.get('http://128.199.169.59/locations')
+    .then(response => (this.locations = response.data))
+    .catch(error => {console.log(error)})
   }
 }
 </script>
